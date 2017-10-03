feeae7b77fbd14564e2611eff56f16849c05c8ee
scott-zph <32261060+scott-zph@users.noreply.github.com>
2017-10-03 09:17:08 +0800

    Delete mnkl1.m

--- mnkl1.m
+++
@@ -1,5 +0,0 @@
-function [m,n,k,l1] = mnkl1(M,C,K,F,u,omega,t,l,x1,x2,y1,y2)
-m = y1*g1(t,omega,l,x1,x2);
-n = y2*g1(t,omega,l,x1,x2);
-k = [y1;y2];
-l1 = -M^(-1)*C*[y1;y2] -M^(-1)*K*[x1;x2] + M^(-1)*F + M^(-1)*u;
