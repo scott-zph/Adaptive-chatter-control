5eb69f29d61e3c4a45f55645fb03b3816d0e233f
scott-zph <32261060+scott-zph@users.noreply.github.com>
2017-10-03 09:17:14 +0800

    Delete mnkl2.m

--- mnkl2.m
+++
@@ -1,5 +0,0 @@
-function [m,n,k,l2] = mnkl2(M,C,K,F,u,omega,t,l,x1,x2,x3,x4,y1,y2)
-m = y1*g2(t,omega,l,x1,x2,x3,x4);
-n = y2*g2(t,omega,l,x1,x2,x3,x4);
-k = [y1;y2];
-l2 = -M^(-1)*C*[y1;y2] -M^(-1)*K*[x2;x4] + M^(-1)*F + M^(-1)*u;
